70459233dc79363fbdc01bb098629f4a279d3356
Sivasini Netra <45381005+sivasini@users.noreply.github.com>
2021-04-25 13:52:59 +0530

    Delete Flask directory

--- Flask/temp.py
+++
@@ -1,8 +0,0 @@
-import cv2
-video_capture = cv2.VideoCapture(0)
-while True:
-        # grab the frame from the threaded video stream
-        ret, frame = video_capture.read()
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        # convert the input frame from BGR to RGB 
-        rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
